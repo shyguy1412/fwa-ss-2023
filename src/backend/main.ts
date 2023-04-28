b5ad19a3596d9e9ce39595d91af5f265918bd78b
Nils Ramstoeck <inf3622@hs-worms.de>
2023-04-28 19:17:53 +0100

    refactored express

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,25 +1,11 @@
-import express from 'express';
+import express from '@/modules/express';
 import { config as loadConfigFile } from 'dotenv';
-import ShopAPI from './modules/ShopAPI';
-import path from 'path';
 
 loadConfigFile();
 
 const app = express();
-const router = express.Router();
 const port = process.env.EXPRESS_PORT || 3000;
-const api_path = `/${process.env.API_PREFIX || 'api'}/${process.env.API_VERSION || 'v0'}`;
-
-app.use('/', express.static('dist/public'));
-app.use(api_path, router);
-
-app.use('/', (_req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
-
 
 app.listen(port, () => {
   console.log(`✅ Express server listening on port ${port}`);
 });
-
-ShopAPI(router);
