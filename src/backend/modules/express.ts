2d54b5650c7556e00f9aa4b0f3bfd9517ba2f7be
NilsRamstoeck <inf3622@hs-worms.de>
2023-07-02 15:03:45 +0200

    setup cors

--- src/backend/modules/express.ts
+++ src/backend/modules/express.ts
@@ -2,17 +2,24 @@ import express from 'express';
 import ShopAPI from './ShopAPI';
 import path from 'path';
 import bodyParser from 'body-parser';
+import { parse } from 'url';
 
 export default function () {
   const app = express();
-  
+
+  app.use((req, res, next) => {
+    const allow = req.hostname == 'localhost';
+    if (allow) res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
   app.use('/', express.static('dist/public'));
   app.use(bodyParser.json());
   app.use(ShopAPI());
-  
+
   app.use('/', (_req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  })
+  });
 
   // app.use((req, res) => res.status(404));
   return app;
