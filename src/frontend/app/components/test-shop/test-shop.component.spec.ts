b6bb80188793de3db600e926a8fd41a3e915444d
TheFlow3107 <florian.czech00@gmx.de>
2023-07-02 11:51:55 +0200

    fixed Unit-test error for test-shop-component

--- src/frontend/app/components/test-shop/test-shop.component.spec.ts
+++ src/frontend/app/components/test-shop/test-shop.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TestShopComponent } from './test-shop.component';
+import { FormsModule } from '@angular/forms';
 
 describe('TestShopComponent', () => {
   let component: TestShopComponent;
@@ -8,10 +8,12 @@ describe('TestShopComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TestShopComponent ]
-    })
-    .compileComponents();
+      declarations: [TestShopComponent],
+      imports: [FormsModule],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(TestShopComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
